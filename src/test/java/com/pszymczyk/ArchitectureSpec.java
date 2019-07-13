de2f051af95cf7db846f693cb84d6fb36f8b88de
Pawel Szymczyk <pawel@pszymczyk.com>
2019-07-13 10:01:08 +0200

    fix tests

--- src/test/java/com/pszymczyk/ArchitectureSpec.java
+++ src/test/java/com/pszymczyk/ArchitectureSpec.java
@@ -9,31 +9,41 @@ import com.tngtech.archunit.core.importer.ClassFileImporter;
 import com.tngtech.archunit.lang.ArchRule;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
 
 public class ArchitectureSpec {
 
     private static final DescribedPredicate<JavaAnnotation> SPRING_ANNOTATIONS = new DescribedPredicate<JavaAnnotation>("annotation from org.springframework") {
         @Override
         public boolean apply(JavaAnnotation input) {
-            return !input.getClass()
+            return input.getRawType()
                          .getPackage()
                          .getName()
                          .startsWith("org.springframework");
         }
     };
+
+    private static final DescribedPredicate<JavaAnnotation> JAVAX_PERSISTENCE_ANNOTATIONS = new DescribedPredicate<JavaAnnotation>("annotation from javax.persistence") {
+        @Override
+        public boolean apply(JavaAnnotation input) {
+            return input.getRawType()
+                         .getPackage()
+                         .getName()
+                         .startsWith("javax.persistence");
+        }
+    };
+
     JavaClasses importedClasses = new ClassFileImporter().withImportOption(location -> !location.contains("/test/"))
                                                          .importPackages("com.pszymczyk");
 
     @Test
     public void domainShouldNotDependOnApplicationAndPorts() {
         //when
-        ArchRule rule = classes().that()
+        ArchRule rule = noClasses().that()
                                  .resideInAPackage("com.pszymczyk.domain")
                                  .should()
-                                 .onlyAccessClassesThat()
-                                 .resideInAnyPackage("com.pszymczyk.domain..", "java..")
-                                 .andShould()
-                                 .notBeAnnotatedWith(SPRING_ANNOTATIONS);
+                                 .dependOnClassesThat()
+                                 .resideInAnyPackage("com.pszymczyk.application..", "com.pszymczyk.adapters..");
 
         //then
         rule.check(importedClasses);
@@ -45,9 +55,31 @@ public class ArchitectureSpec {
         ArchRule rule = classes().that()
                                  .resideInAPackage("com.pszymczyk.application")
                                  .should()
-                                 .onlyAccessClassesThat()
-                                 .resideInAnyPackage("com.pszymczyk.domain..", "java..")
-                                 .andShould()
+                                 .onlyDependOnClassesThat()
+                                 .resideInAnyPackage("com.pszymczyk.domain..", "java..");
+
+        //then
+        rule.check(importedClasses);
+    }
+
+    @Test
+    public void domainShouldNotBeAnnotatedWithJavaxPersistence() {
+        //when
+        ArchRule rule = classes().that()
+                                 .resideInAPackage("com.pszymczyk.domain")
+                                 .should()
+                                 .notBeAnnotatedWith(JAVAX_PERSISTENCE_ANNOTATIONS);
+
+        //then
+        rule.check(importedClasses);
+    }
+
+    @Test
+    public void applicationShouldNotBeAnnotatedWithSpring() {
+        //when
+        ArchRule rule = classes().that()
+                                 .resideInAPackage("com.pszymczyk.application")
+                                 .should()
                                  .notBeAnnotatedWith(SPRING_ANNOTATIONS);
 
         //then
