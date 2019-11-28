14c7e37eda672b5eac81b73473b0e7a44f3e9c77
Pawel Szymczyk <pawel@pszymczyk.com>
2019-11-28 14:23:31 +0100

    improve spring data

--- src/main/java/com/pszymczyk/adapter/persistence/CrudTripRepository.java
+++ src/main/java/com/pszymczyk/adapter/persistence/CrudTripRepository.java
@@ -1,9 +1,9 @@
 package com.pszymczyk.adapter.persistence;
 
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
-interface CrudTripRepository extends PagingAndSortingRepository<TripEntity, Long> {
+interface CrudTripRepository extends CrudRepository<TripEntity, Long> {
 
     TripEntity findByTripCode(@Param("tripCode") String tripCode);
 
