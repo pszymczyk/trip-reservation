e556b24cad0467b91c778dd56f5bd12c68574966
Pawel Szymczyk <pawel@pszymczyk.com>
2019-12-06 19:48:15 +0100

    step5

--- src/main/java/com/pszymczyk/ApplicationConfiguration.java
+++ src/main/java/com/pszymczyk/ApplicationConfiguration.java
@@ -2,14 +2,12 @@ package com.pszymczyk;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @Configuration
-@EnableAsync
 class ApplicationConfiguration {
 
     @Bean
-    TripService tripService(TripRepository tripRepository) {
-        return new TripService(tripRepository);
+    TripService tripService(TripRepository tripRepository, ReservationsReadModel reservationsReadModel) {
+        return new TripService(tripRepository, reservationsReadModel);
     }
 }
