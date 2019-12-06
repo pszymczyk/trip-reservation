43ff7c41ef0b57faf147b889124a7c0352edbb08
Pawel Szymczyk <pawel@pszymczyk.com>
2019-12-06 19:32:13 +0100

    step4

---
+++ src/main/java/com/pszymczyk/TripCatalogueEntity.java
@@ -0,0 +1,71 @@
+package com.pszymczyk;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+class TripCatalogueEntity {
+
+    @Id
+    @GeneratedValue
+    private long entityId;
+
+    private String tripCode;
+
+    private int seatsNumber;
+
+    private String shortDescription;
+
+    private String description;
+
+    private String destination;
+
+    public long getEntityId() {
+        return entityId;
+    }
+
+    public void setEntityId(long entityId) {
+        this.entityId = entityId;
+    }
+
+    public String getTripCode() {
+        return tripCode;
+    }
+
+    public void setTripCode(String tripCode) {
+        this.tripCode = tripCode;
+    }
+
+    public int getSeatsNumber() {
+        return seatsNumber;
+    }
+
+    public void setSeatsNumber(int seatsNumber) {
+        this.seatsNumber = seatsNumber;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+}
